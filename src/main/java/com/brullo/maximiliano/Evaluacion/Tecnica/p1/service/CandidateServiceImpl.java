4d72d2d646386d47fb68e878f8b0cd425ec75323
Maximiliano Brullo <maxibrullo@outlook.com>
2022-09-12 15:50:23 -0300

    add '@Transactional' annotation

--- src/main/java/com/brullo/maximiliano/Evaluacion/Tecnica/p1/service/CandidateServiceImpl.java
+++ src/main/java/com/brullo/maximiliano/Evaluacion/Tecnica/p1/service/CandidateServiceImpl.java
@@ -3,6 +3,7 @@ package com.brullo.maximiliano.Evaluacion.Tecnica.p1.service;
 import com.brullo.maximiliano.Evaluacion.Tecnica.p1.dao.CandidateDao;
 import com.brullo.maximiliano.Evaluacion.Tecnica.p1.domain.Candidate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -12,21 +13,25 @@ public class CandidateServiceImpl implements CandidateService{
     private CandidateDao candidateDao;
 
     @Override
+    @Transactional(readOnly = true)
     public List<Candidate> listCandidate(){
         return (List<Candidate>) candidateDao.findAll();
     }
 
     @Override
+    @Transactional
     public void save(Candidate candidate){
         candidateDao.delete(candidate);
     }
 
     @Override
+    @Transactional
     public void delete(Candidate candidate){
         candidateDao.delete(candidate);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public Candidate findCandidate(Candidate candidate){
         return candidateDao.findById(candidate.getCandidateId()).orElse(null);
     }
