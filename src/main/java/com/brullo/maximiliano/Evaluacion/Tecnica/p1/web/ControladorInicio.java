3e1a95d9f7d3d058c1dadee7299b00bfe8543110
Maximiliano Brullo <maxibrullo@outlook.com>
2022-09-12 16:46:24 -0300

    change 'CandidateDao' by 'CandidateService'

--- src/main/java/com/brullo/maximiliano/Evaluacion/Tecnica/p1/web/ControladorInicio.java
+++ src/main/java/com/brullo/maximiliano/Evaluacion/Tecnica/p1/web/ControladorInicio.java
@@ -1,6 +1,6 @@
 package com.brullo.maximiliano.Evaluacion.Tecnica.p1.web;
 
-import com.brullo.maximiliano.Evaluacion.Tecnica.p1.dao.CandidateDao;
+import com.brullo.maximiliano.Evaluacion.Tecnica.p1.service.CandidateService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
@@ -12,12 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class ControladorInicio {
 
     @Autowired
-    private CandidateDao candidateDao;
+    private CandidateService candidateService;
 
     @GetMapping("/")
     public String inicio(Model model){
 
-        var candidate = candidateDao.findAll();
+        var candidate = candidateService.listCandidate();
 
         log.info("Controlador MVC...");
         return "index";
