8999866cfb6bca66f64bb152a4e2a468063fafd9
Maximiliano Brullo <maxibrullo@outlook.com>
2022-09-12 10:22:24 -0300

    first commit

---
+++ src/main/java/com/brullo/maximiliano/Evaluacion/Tecnica/p1/EvaluacionTecnicaP1Application.java
@@ -0,0 +1,13 @@
+package com.brullo.maximiliano.Evaluacion.Tecnica.p1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaluacionTecnicaP1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvaluacionTecnicaP1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/brullo/maximiliano/Evaluacion/Tecnica/p1/EvaluacionTecnicaP1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brullo.maximiliano.Evaluacion.Tecnica.p1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvaluacionTecnicaP1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
