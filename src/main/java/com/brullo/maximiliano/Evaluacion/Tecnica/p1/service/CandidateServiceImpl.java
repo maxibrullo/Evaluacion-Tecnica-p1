8f2b3b12c40f65d293a5c9731fdc3b1776b5d2fd
Maximiliano Brullo <maxibrullo@outlook.com>
2022-09-12 15:32:23 -0300

    add methods 'lisCandidate', 'save', 'delete', 'findCandidate'

--- src/main/java/com/brullo/maximiliano/Evaluacion/Tecnica/p1/service/CandidateServiceImpl.java
+++ src/main/java/com/brullo/maximiliano/Evaluacion/Tecnica/p1/service/CandidateServiceImpl.java
@@ -1,4 +1,30 @@
 package com.brullo.maximiliano.Evaluacion.Tecnica.p1.service;
 
-public class CandidateServiceImpl {
+import com.brullo.maximiliano.Evaluacion.Tecnica.p1.dao.CandidateDao;
+import com.brullo.maximiliano.Evaluacion.Tecnica.p1.domain.Candidate;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class CandidateServiceImpl implements CandidateService{
+
+    @Autowired
+    private CandidateDao candidateDao;
+
+    @Override
+    public List<Candidate> listCandidate(){
+        return (List<Candidate>) candidateDao.findAll();
+    }
+
+    public void save(Candidate candidate){
+        candidateDao.delete(candidate);
+    }
+
+    public void delete(Candidate candidate){
+        candidateDao.delete(candidate);
+    }
+
+    public Candidate findCandidate(Candidate candidate){
+        return candidateDao.findById(candidate.getCandidateId()).orElse(null);
+    }
 }
