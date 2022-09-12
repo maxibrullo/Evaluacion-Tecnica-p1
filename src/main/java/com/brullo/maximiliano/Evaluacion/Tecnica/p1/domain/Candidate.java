04928e7aaea3cdc972eb35ebe4a20016b0d55550
Maximiliano Brullo <maxibrullo@outlook.com>
2022-09-12 11:58:36 -0300

    crates Candidate class

--- src/main/java/com/brullo/maximiliano/Evaluacion/Tecnica/p1/domain/Candidate.java
+++ src/main/java/com/brullo/maximiliano/Evaluacion/Tecnica/p1/domain/Candidate.java
@@ -1,6 +1,25 @@
 package com.brullo.maximiliano.Evaluacion.Tecnica.p1.domain;
 
+
+
+import lombok.Data;
+
+import javax.persistence.*;
 import java.io.Serializable;
 
+@Data
+@Entity
+@Table(name = "candidate")
+
 public class Candidate implements Serializable {
+
+    private static final long serialVersionUID = 1l;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long candidateId;
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String telefono;
 }
