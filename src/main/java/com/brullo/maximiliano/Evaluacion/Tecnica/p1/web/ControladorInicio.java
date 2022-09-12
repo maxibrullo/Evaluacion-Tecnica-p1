7978a15482362757635ab62f01caab206c19a412
Maximiliano Brullo <maxibrullo@outlook.com>
2022-09-12 12:28:34 -0300

    injects CandidateDao in ControladorInicio, and add 'var candidate'

--- src/main/java/com/brullo/maximiliano/Evaluacion/Tecnica/p1/web/ControladorInicio.java
+++ src/main/java/com/brullo/maximiliano/Evaluacion/Tecnica/p1/web/ControladorInicio.java
@@ -1,6 +1,8 @@
 package com.brullo.maximiliano.Evaluacion.Tecnica.p1.web;
 
+import com.brullo.maximiliano.Evaluacion.Tecnica.p1.dao.CandidateDao;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class ControladorInicio {
 
+    @Autowired
+    private CandidateDao candidateDao;
+
     @GetMapping("/")
     public String inicio(Model model){
+
+        var candidate = candidateDao.findAll();
+
         log.info("Controlador MVC...");
         return "index";
     }
