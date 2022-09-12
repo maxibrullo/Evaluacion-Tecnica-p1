1a4f2cb28ba2308907d987f7d1763b993cbadfb6
Maximiliano Brullo <maxibrullo@outlook.com>
2022-09-12 10:39:39 -0300

    add Candidate class and Technology class

---
+++ src/main/java/com/brullo/maximiliano/Evaluacion/Tecnica/p1/domain/Candidate.java
@@ -0,0 +1,6 @@
+package com.brullo.maximiliano.Evaluacion.Tecnica.p1.domain;
+
+import java.io.Serializable;
+
+public class Candidate implements Serializable {
+}
---
+++ src/main/java/com/brullo/maximiliano/Evaluacion/Tecnica/p1/domain/Technology.java
@@ -0,0 +1,6 @@
+package com.brullo.maximiliano.Evaluacion.Tecnica.p1.domain;
+
+import java.io.Serializable;
+
+public class Technology implements Serializable {
+}
