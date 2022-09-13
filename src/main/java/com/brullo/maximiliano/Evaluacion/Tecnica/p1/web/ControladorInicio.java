49d152b29e224d4b6f3775a98b4c4c29372b3f5e
Maximiliano Brullo <maxibrullo@outlook.com>
2022-09-13 10:20:38 -0300

    creates guardar method

--- src/main/java/com/brullo/maximiliano/Evaluacion/Tecnica/p1/web/ControladorInicio.java
+++ src/main/java/com/brullo/maximiliano/Evaluacion/Tecnica/p1/web/ControladorInicio.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -24,8 +25,14 @@ public class ControladorInicio {
         return "index";
     }
 
+    @GetMapping("/agregar")
     public String agregar(Candidate candidate) {
         return "modificar";
     }
 
+    @PostMapping("/guardar")
+    public String guardar(Candidate candidate){
+        candidateService.save(candidate);
+        return "redirect:/";
+    }
 }
